cf05626e211992d80f3e7b722b8f3bfdf29b04ec
eduardo.brito <eduardo.brito@plannersistemas.com.br>
2024-07-10 16:21:09 -0300

    feat: create h1 and p text

---
+++ app.js
@@ -0,0 +1,5 @@
+let titulo = document.querySelector('h1')
+titulo.innerHTML = 'SECRET NUMBER GAME'
+
+let paragraph = document.querySelector('p')
+paragraph.innerHTML = 'Pick a number between 1 and 10'
